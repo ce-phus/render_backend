abba6e4203f316cf1fc84525e0d2384116479e8b
cephus <pnakitaren2@gmail.com>
2024-11-25 10:05:04 -0500

    task:removed super user config

--- build.sh
+++ build.sh
@@ -8,12 +8,12 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-if [[ $CREATE_SUPERUSER ]]; then
-    python manage.py createsuperuser \
-        --no-input \
-        --username "$DJANGO_SUPERUSER_USERNAME" \
-        --email "$DJANGO_SUPERUSER_EMAIL" \
-        --first_name "$DJANGO_SUPERUSER_FIRST_NAME" \
-        --last_name "$DJANGO_SUPERUSER_LAST_NAME"
-fi
+# if [[ $CREATE_SUPERUSER ]]; then
+#     python manage.py createsuperuser \
+#         --no-input \
+#         --username "$DJANGO_SUPERUSER_USERNAME" \
+#         --email "$DJANGO_SUPERUSER_EMAIL" \
+#         --first_name "$DJANGO_SUPERUSER_FIRST_NAME" \
+#         --last_name "$DJANGO_SUPERUSER_LAST_NAME"
+# fi
 
