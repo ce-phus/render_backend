246885870e94e1026628de4310e94f8813f6b548
cephus <pnakitaren2@gmail.com>
2024-11-25 07:58:53 -0500

    task:django setup

--- hello_tractor/asgi.py
+++ hello_tractor/asgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
+import django
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -17,6 +18,7 @@ from apps.chats.channels_middleware import JWTWebsocketMiddleware
 
 settings_module = 'hello_tractor.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'hello_tractor.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
