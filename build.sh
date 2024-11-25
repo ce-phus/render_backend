c5819a52fe4ac355ba666dff57f6a15ebb647c8f
cephus <pnakitaren2@gmail.com>
2024-11-25 08:26:28 -0500

    task:Gunicorn server and django setup config

--- build.sh
+++ build.sh
@@ -1,10 +1,16 @@
 set -o errexit
 
+# Set the DJANGO_SETTINGS_MODULE explicitly for the Gunicorn process
 echo "Setting DJANGO_SETTINGS_MODULE..."
 export DJANGO_SETTINGS_MODULE=hello_tractor.deployment_settings
 
+# Install dependencies
 pip install -r requirements.txt
 
+# Run migrations and collect static files
 python manage.py collectstatic --no-input
-
 python manage.py migrate
+
+# Finally, start Gunicorn server with Uvicorn worker
+echo "Starting Gunicorn server..."
+exec gunicorn hello_tractor.asgi:application -k uvicorn.workers.UvicornWorker
--- hello_tractor/asgi.py
+++ hello_tractor/asgi.py
@@ -1,25 +1,19 @@
-"""
-ASGI config for hello_tractor project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
 import os
 import django
-
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from apps.chats.routing import websocket_urlpatterns
 from apps.chats.channels_middleware import JWTWebsocketMiddleware
 
+# Setting the environment variable for the settings module
 settings_module = 'hello_tractor.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'hello_tractor.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+
+# Initialize Django settings and ensure apps are loaded
 django.setup()
 
+# Define the ASGI application
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": JWTWebsocketMiddleware(
