3478987334d39d7fcb08c78fc10e8be91671e058
cephus <pnakitaren2@gmail.com>
2024-11-25 07:45:17 -0500

    chore:django module settings

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 set -o errexit
 
+echo "Setting DJANGO_SETTINGS_MODULE..."
+export DJANGO_SETTINGS_MODULE=hello_tractor.deployment_settings
+
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
