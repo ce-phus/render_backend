4a7e6c0c02db69663077f302ecae56413c4c19f6
cephus <pnakitaren2@gmail.com>
2024-11-25 09:15:54 -0500

    task:build.sh config

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 set -o errexit
 
-# Set the DJANGO_SETTINGS_MODULE explicitly for the Gunicorn process
-echo "Setting DJANGO_SETTINGS_MODULE..."
-export DJANGO_SETTINGS_MODULE=hello_tractor.deployment_settings
 
 # Install dependencies
 pip install -r requirements.txt
@@ -11,7 +8,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-# Finally, start Gunicorn server with Uvicorn worker
-echo "Starting Gunicorn server..."
-exec gunicorn hello_tractor.asgi:application -k uvicorn.workers.UvicornWorker --log-level info
+
 
